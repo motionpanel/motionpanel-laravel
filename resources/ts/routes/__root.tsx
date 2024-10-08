7ef3ff7d320014e254342735a03e30f329df25d6
Tony Song <tonywei92@gmail.com>
2024-10-08 16:11:18 +0700

    chore(tanstack router): update tanstack to load only in non-production environment

--- resources/ts/routes/__root.tsx
+++ resources/ts/routes/__root.tsx
@@ -1,9 +1,21 @@
+import React, { Suspense } from "react";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Toaster } from "@/components/ui/sonner";
 import { HOMEPAGE_ROOT_PATH } from "@/config/config";
 import { HomeIcon, ListIcon, TriangleAlert } from "lucide-react";
 import MotionPanelLogoSvg from "../../assets/motion-panel-logo-2.svg?react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
 
 const menus = [
   {
@@ -51,7 +63,9 @@ export const Route = createRootRoute({
         </div>
       </div>
       <Toaster />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   ),
 });
