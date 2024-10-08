d400e121fb91b36327e73d486fe1b7ef05d559fe
Tony Song <tonywei92@gmail.com>
2024-10-08 12:48:49 +0700

    feat: add detailed view for a Job

--- resources/ts/routes/jobs/failed/index.lazy.tsx
+++ resources/ts/routes/jobs/failed/index.lazy.tsx
@@ -6,12 +6,21 @@ import { HOMEPAGE_ROOT_PATH } from "@/config/config";
 import type { HttpResponse } from "@/types";
 import { PageHeader } from "@/components/ui/page-header";
 import { EmptyPagePlaceholder } from "@/components/ui/empty-page-placeholder";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { JobView } from "@/modules/Job/job-view";
 
 export const Route = createLazyFileRoute("/jobs/failed/")({
   component: Jobs,
 });
 
 function Jobs() {
+  const [selectedJob, setSelectedJob] = useState<Job | null>(null);
   const jobsQuery = useQuery<HttpResponse<Job[]>>({
     queryKey: ["failed-jobs"],
     queryFn: async () => {
@@ -53,33 +62,57 @@ function Jobs() {
   });
   return (
     <div className="w-full">
-      <PageHeader>
-        <h1 className="font-medium">Failed Jobs</h1>
-      </PageHeader>
-      <div className="flex flex-col space-y-2">
-        {jobsQuery.isLoading && <div>Loading...</div>}
-        {jobsQuery.isError && <div>Error: {jobsQuery.error.message}</div>}
-        {jobsQuery.isSuccess && jobsQuery.data?.data?.length === 0 && (
-          <div className="flex items-center justify-center">
-            <EmptyPagePlaceholder>
-              <div className="text-lg font-extralight">
-                Congratulations, no failed jobs!
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel>
+          <PageHeader>
+            <h1 className="font-medium">Failed Jobs</h1>
+          </PageHeader>
+          <div className="flex flex-col space-y-2 p-4">
+            {jobsQuery.isLoading && <div>Loading...</div>}
+            {jobsQuery.isError && <div>Error: {jobsQuery.error.message}</div>}
+            {jobsQuery.isSuccess && jobsQuery.data?.data?.length === 0 && (
+              <div className="flex items-center justify-center">
+                <EmptyPagePlaceholder>
+                  <div className="text-lg font-extralight">
+                    Congratulations, no failed jobs!
+                  </div>
+                </EmptyPagePlaceholder>
               </div>
-            </EmptyPagePlaceholder>
+            )}
+            {jobsQuery.isSuccess &&
+              jobsQuery.data?.data?.map((job) => (
+                <ListItem
+                  key={job.id}
+                  job={job}
+                  className={cn(
+                    selectedJob?.id === job.id ? "bg-zinc-100" : undefined,
+                    "border",
+                    "border-zinc-200",
+                    "rounded-sm"
+                  )}
+                  onDelete={() => deleteJobFailedMutation.mutate(job.id)}
+                  onRetry={() => retryJobMutation.mutate(job.id)}
+                  showRetryButton
+                  showDeleteButton
+                  showFailedAt
+                  onItemClick={() => setSelectedJob(job)}
+                />
+              ))}
           </div>
-        )}
-        {jobsQuery.isSuccess &&
-          jobsQuery.data?.data?.map((job) => (
-            <ListItem
-              key={job.id}
-              job={job}
-              onDelete={() => deleteJobFailedMutation.mutate(job.id)}
-              onRetry={() => retryJobMutation.mutate(job.id)}
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>
+          {selectedJob && (
+            <JobView
+              job={selectedJob}
               showRetryButton
-              showDeleteButton
+              showDeleteButton={!selectedJob.reserved_at}
+              onRetry={() => retryJobMutation.mutate(selectedJob.id)}
+              onDelete={() => deleteJobFailedMutation.mutate(selectedJob.id)}
             />
-          ))}
-      </div>
+          )}
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
--- resources/ts/routes/jobs/index.lazy.tsx
+++ resources/ts/routes/jobs/index.lazy.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ListItem, Job } from "@/modules/Job";
 import { toast } from "sonner";
 import { useMutation, useQuery } from "@tanstack/react-query";
@@ -6,12 +7,20 @@ import { HOMEPAGE_ROOT_PATH } from "@/config/config";
 import type { HttpResponse } from "@/types";
 import { PageHeader } from "@/components/ui/page-header";
 import { EmptyPagePlaceholder } from "@/components/ui/empty-page-placeholder";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { JobView } from "@/modules/Job/job-view";
+import { cn } from "@/lib/utils";
 
 export const Route = createLazyFileRoute("/jobs/")({
   component: Jobs,
 });
 
 function Jobs() {
+  const [selectedJob, setSelectedJob] = useState<Job | null>(null);
   const jobsQuery = useQuery<HttpResponse<Job[]>>({
     queryKey: ["jobs"],
     queryFn: async () => {
@@ -34,38 +43,60 @@ function Jobs() {
   });
 
   return (
-    <div className="w-full">
-      <PageHeader>
-        <h1 className="font-medium">Jobs</h1>
-      </PageHeader>
-      {jobsQuery.isLoading && <div>Loading...</div>}
-      {jobsQuery.isError && <div>Error: {jobsQuery.error.message}</div>}
-      {jobsQuery.isSuccess && jobsQuery.data?.data?.length === 0 && (
-        <div className="flex items-center justify-center">
-          <EmptyPagePlaceholder>
-            <div className="text-lg font-extralight">No jobs available</div>
-          </EmptyPagePlaceholder>
-        </div>
-      )}
-      <div className="flex flex-col divide-y">
-        {jobsQuery.isSuccess &&
-          jobsQuery.data?.data?.map((job) => (
-            <ListItem
-              className="px-4 py-4"
-              key={job.id}
-              job={job}
-              showAttempts
-              showCreatedAt
-              showDeleteButton={!job.reserved_at}
-              onDelete={() => deleteJobFailedMutation.mutate(job.id)}
-              description={
-                job.reserved_at
-                  ? "Possibly in progress or will be retried"
-                  : undefined
-              }
+    <div className="w-full h-full flex flex-col">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel>
+          <PageHeader>
+            <h1 className="font-medium">Jobs</h1>
+          </PageHeader>
+          <div className="flex flex-col space-y-2 p-4">
+            {jobsQuery.isLoading && <div>Loading...</div>}
+            {jobsQuery.isError && <div>Error: {jobsQuery.error.message}</div>}
+            {jobsQuery.isSuccess && jobsQuery.data?.data?.length === 0 && (
+              <div className="flex items-center justify-center">
+                <EmptyPagePlaceholder>
+                  <div className="text-lg font-extralight">
+                    No jobs available
+                  </div>
+                </EmptyPagePlaceholder>
+              </div>
+            )}
+
+            {jobsQuery.isSuccess &&
+              jobsQuery.data?.data?.map((job) => (
+                <div className="rounded-sm" key={job.id}>
+                  <ListItem
+                    className={cn(
+                      selectedJob?.id === job.id ? "bg-zinc-100" : undefined,
+                      "border",
+                      "border-zinc-200"
+                    )}
+                    job={job}
+                    showAttempts
+                    showCreatedAt
+                    showDeleteButton={!job.reserved_at}
+                    onDelete={() => deleteJobFailedMutation.mutate(job.id)}
+                    description={
+                      job.reserved_at
+                        ? "Possibly in progress or will be retried"
+                        : undefined
+                    }
+                    onItemClick={() => setSelectedJob(job)}
+                  />
+                </div>
+              ))}
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>
+          {selectedJob && (
+            <JobView
+              job={selectedJob}
+              showDeleteButton={!selectedJob.reserved_at}
             />
-          ))}
-      </div>
+          )}
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
