a480c6b7f24de892e5e8ed1c3f92831ffe83be9f
Tony Song <tonywei92@gmail.com>
2024-10-08 12:49:04 +0700

    feat: add single full job view

---
+++ resources/ts/modules/Job/job-view.tsx
@@ -0,0 +1,84 @@
+import { PageHeader } from "@/components/ui/page-header";
+import type { Job } from "./types";
+import { Button } from "@/components/ui/button";
+import { CopyIcon, RotateCcwIcon, Trash2Icon } from "lucide-react";
+import moment from "moment";
+
+interface Props {
+  job: Job;
+  showDeleteButton?: boolean;
+  showRetryButton?: boolean;
+  onDelete?: (job: Job) => void;
+  onRetry?: (job: Job) => void;
+}
+
+export function JobView(props: Props) {
+  const { job, showDeleteButton, onDelete, showRetryButton, onRetry } = props;
+  const copyJobPayload = () => {
+    navigator.clipboard.writeText(JSON.stringify(job, null, 2));
+  };
+  return (
+    <>
+      <PageHeader>
+        <div className="flex w-full items-center">
+          <div className="flex-1"></div>
+          {showRetryButton && (
+            <Button
+              size="icon"
+              variant="ghost"
+              className="text-indigo-800"
+              onClick={(e) => {
+                e.stopPropagation();
+                onRetry && onRetry(job);
+              }}
+            >
+              <RotateCcwIcon className="w-4 h-4" />
+            </Button>
+          )}
+          {showDeleteButton && (
+            <Button
+              size="icon"
+              variant={"ghost"}
+              className="text-red-800"
+              onClick={(e) => {
+                e.stopPropagation();
+                onDelete && onDelete(job);
+              }}
+            >
+              <Trash2Icon className="w-4 h-4" />
+            </Button>
+          )}
+        </div>
+      </PageHeader>
+      <div>
+        <div className="p-4">
+          <div>ID: {job.id}</div>
+          <div>
+            <div>
+              On <span className="font-semibold">{job.queue}</span> queue
+            </div>
+            <div>
+              At {job.created_at} ({moment(job.created_at).fromNow()})
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="p-4">
+          <div className="">Payload:</div>
+          <div className="relative">
+            <div className="absolute right-0 top-0 p-2 z-30">
+              <Button size="icon" variant={"outline"} onClick={copyJobPayload}>
+                <CopyIcon className="w-4 h-4" />
+              </Button>
+            </div>
+            <div className="flex-1 relative w-full h-[500px] overflow-auto border">
+              <pre className="absolute left-0 right-0 top-0 bottom-0 p-4">
+                {JSON.stringify(job, null, 2)}
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
