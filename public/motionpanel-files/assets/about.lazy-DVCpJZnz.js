c61091cc593581ebf1562cfd78df956578a7c153
Tony Song <tonywei92@gmail.com>
2024-10-10 10:44:57 +0700

    feat: release v0.0.3

---
+++ public/motionpanel-files/assets/about.lazy-DVCpJZnz.js
@@ -0,0 +1,1 @@
+import{c as o,j as t}from"./app-BKI5hTUl.js";const a=o("/about")({component:()=>t.jsx("div",{children:"Hello /about!"})});export{a as Route};
--- public/motionpanel-files/assets/index.lazy-Cqlb6A22.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as e}from"./app-B6RDJg5p.js";import{P as t}from"./page-header-uzg5tLCo.js";const n=o("/")({component:r});function r(){return e.jsx(t,{children:e.jsx("h1",{className:"font-medium",children:"Home"})})}export{n as Route};
